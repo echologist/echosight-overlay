6450e36039eb26d6193fa3fd6a1599d5df6197e7
Flycro <48244006+Flycro@users.noreply.github.com>
2026-04-28 23:36:29 +0200

    chore: type electron vite config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,5 +1,5 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
+import { resolve } from 'node:path';
+import { defineConfig } from 'electron-vite';
 
 export default defineConfig({
   main: {
@@ -23,4 +23,4 @@ export default defineConfig({
       outDir: 'dist-electron/renderer'
     }
   }
-})
+});
